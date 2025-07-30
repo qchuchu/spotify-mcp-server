66898de943c193cd35effddfc3988fabba087f88
Frédéric Barthelet <frederic.barthelet@gmail.com>
2025-07-30 23:37:36 +0200

    Remove session management

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import { StreamableHTTPServerTransport } from "@modelcontextprotocol/sdk/server/streamableHttp.js";
-import { isInitializeRequest } from "@modelcontextprotocol/sdk/types.js";
-import { InMemoryEventStore } from "@modelcontextprotocol/sdk/examples/shared/inMemoryEventStore.js";
 import express, { type Request, type Response } from "express";
-import { randomUUID } from "node:crypto";
 
 import { getServer } from "./server.js";
 import { config } from "./config.js";
@@ -10,46 +7,18 @@ import { config } from "./config.js";
 const app = express();
 app.use(express.json());
 
-type SessionId = string;
-const transports = new Map<SessionId, StreamableHTTPServerTransport>();
-
 app.post("/mcp", async (req: Request, res: Response) => {
-  const sessionId = req.headers["mcp-session-id"] as string | undefined;
   try {
-    let transport: StreamableHTTPServerTransport;
-    if (sessionId && transports.has(sessionId)) {
-      transport = transports.get(sessionId)!;
-    } else if (!sessionId && isInitializeRequest(req.body)) {
-      transport = new StreamableHTTPServerTransport({
-        eventStore: new InMemoryEventStore(),
-        sessionIdGenerator: () => randomUUID(),
-        onsessioninitialized: (sessionId) => {
-          console.log(`Session initialized with ID: ${sessionId}`);
-          transports.set(sessionId, transport);
-        },
-      });
+    const transport = new StreamableHTTPServerTransport({
+      sessionIdGenerator: undefined,
+    });
 
-      transport.onclose = () => {
-        const deleted = transport.sessionId && transports.delete(transport.sessionId);
-        if (deleted) {
-          console.log(`Transport closed for session ${transport.sessionId}, removing from transports map`);
-        }
-      };
+    res.on("close", () => {
+      transport.close();
+    });
 
-      const server = getServer();
-      await server.connect(transport);
-    } else {
-      res.status(400).json({
-        jsonrpc: "2.0",
-        error: {
-          code: -32000,
-          message: "Bad Request: No valid session ID provided",
-        },
-        id: null,
-      });
-
-      return;
-    }
+    const server = getServer();
+    await server.connect(transport);
 
     await transport.handleRequest(req, res, req.body);
   } catch (error) {
@@ -68,42 +37,31 @@ app.post("/mcp", async (req: Request, res: Response) => {
 });
 
 app.get("/mcp", async (req: Request, res: Response) => {
-  const sessionId = req.headers["mcp-session-id"] as string | undefined;
-  if (!sessionId) {
-    res.status(400).send("Invalid or missing session ID");
-    return;
-  }
-
-  const transport = transports.get(sessionId);
-  if (!transport) {
-    res.status(404).send("Session not found");
-    return;
-  }
-
-  await transport.handleRequest(req, res);
+  console.log("Received GET MCP request");
+  res.writeHead(405).end(
+    JSON.stringify({
+      jsonrpc: "2.0",
+      error: {
+        code: -32000,
+        message: "Method not allowed.",
+      },
+      id: null,
+    }),
+  );
 });
 
 app.delete("/mcp", async (req: Request, res: Response) => {
-  const sessionId = req.headers["mcp-session-id"] as string | undefined;
-  if (!sessionId) {
-    res.status(400).send("Invalid or missing session ID");
-    return;
-  }
-
-  const transport = transports.get(sessionId);
-  if (!transport) {
-    res.status(404).send("Session not found");
-    return;
-  }
-
-  try {
-    await transport.handleRequest(req, res);
-  } catch (error) {
-    console.error("Error handling session termination:", error);
-    if (!res.headersSent) {
-      res.status(500).send("Error processing session termination");
-    }
-  }
+  console.log("Received GET MCP request");
+  res.writeHead(405).end(
+    JSON.stringify({
+      jsonrpc: "2.0",
+      error: {
+        code: -32000,
+        message: "Method not allowed.",
+      },
+      id: null,
+    }),
+  );
 });
 
 app.listen(config.MCP_HTTP_PORT, (error) => {
@@ -115,14 +73,6 @@ app.listen(config.MCP_HTTP_PORT, (error) => {
 });
 
 process.on("SIGINT", async () => {
-  transports.forEach(async (transport, sessionId) => {
-    try {
-      await transport.close();
-      transports.delete(sessionId);
-    } catch (error) {
-      console.error(`Error closing transport for session ${sessionId}:`, error);
-    }
-  });
   console.log("Server shutdown complete");
   process.exit(0);
 });
