6095db0205450b0c64ae7c04480eaaaba7d8b2d9
Quentin Churet <quentin@phacetlabs.com>
2025-09-20 19:51:43 +0200

    feat: revert to normal client secret post

--- src/index.ts
+++ src/index.ts
@@ -91,7 +91,7 @@ app.use(
         "user-library-read",
       ],
       code_challenge_methods_supported: ["S256"],
-      token_endpoint_auth_methods_supported: ["none"],
+      token_endpoint_auth_methods_supported: ["client_secret_post"],
     },
     resourceServerUrl: new URL("http://localhost:3000"),
   }),
